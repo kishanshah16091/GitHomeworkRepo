0aa2d434c30bbb9c9dbe6eb8a9fdfffc06c35180
kishanshah16091 <kishan.shah16091@gmail.com>
2022-05-14 15:50:23 -0400

    git homework

---
+++ src/GitHomework/HW1.java
@@ -0,0 +1,11 @@
+package GitHomework;
+
+public class HW1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+
+            System.out.println(i);
+        }
+    }
+}
+
